093d6681499273ac0438550d6ae2ca463608cdf6
Samuel Dantas de Oliveira <Samuel.0913@outlook.com>
2025-06-26 18:49:47 -0300

    refactor: add generic OperationResult with support for return value

--- Taskly.API/Application/Results/OperationResult.cs
+++ Taskly.API/Application/Results/OperationResult.cs
@@ -6,5 +6,17 @@ public class OperationResult<TFailureReason>
     public static OperationResult<TFailureReason> Fail(TFailureReason reason) =>
     new() { FailureReason = reason };
     public static OperationResult<TFailureReason> Ok() => new();
+}
+public class OperationResult<TFailureReason, TValue>
+    where TFailureReason : struct, Enum
+{
+    public bool Success => EqualityComparer<TFailureReason>.Default.Equals(FailureReason, default);
+    public TFailureReason FailureReason { get; init; } = default;
+    public TValue? Value { get; init; }
+
+    public static OperationResult<TFailureReason, TValue> Fail(TFailureReason reason) =>
+    new() { FailureReason = reason };
+    public static OperationResult<TFailureReason, TValue> Ok(TValue value) =>
+    new() { Value = value };
 
 }
