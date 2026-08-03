10cc91375e4502a7061c2a50b65f8b180ece1215
Samuel Dantas de Oliveira <Samuel.0913@outlook.com>
2026-08-03 15:56:32 -0300

    refactor: add non-generic StructuredOperationResult

--- Taskly.API/Application/Results/StructuredOperationResult.cs
+++ Taskly.API/Application/Results/StructuredOperationResult.cs
@@ -1,13 +1,24 @@
 using Taskly.Application.Results;
 
-public class StructuredOperationResult<T>
+public class StructuredOperationResult
 {
     public bool Success { get; init; }
-    public T? Value { get; init; }
     public Error? Error { get; init; }
 
+    public static StructuredOperationResult Ok() => new() {Success = true};
+
+    public static StructuredOperationResult Fail(Error error)
+    {
+        if (error is null) throw new ArgumentNullException(nameof(error));
+        return new() {Success = false, Error = error};
+    } 
+}
+public class StructuredOperationResult<T> : StructuredOperationResult
+{
+    public T? Value { get; init; }
+
     public static StructuredOperationResult<T> Ok(T value) => new() {Success = true, Value = value};
-    public static StructuredOperationResult<T> Fail(Error error)
+    public new static StructuredOperationResult<T> Fail(Error error)
     {
         if (error is null) throw new ArgumentNullException(nameof(error));
         return new() {Success = false, Error = error};
